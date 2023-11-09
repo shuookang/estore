f6b013c2368267b71ba2452aef4ceaf68e62a961
feliciaaong <feliciaong444@gmail.com>
2023-11-09 15:08:04 +0800

    Create dbinfo.php

---
+++ dbinfo.php
@@ -0,0 +1,12 @@
+<?php
+
+    //Set up 4 global variables that can be included by other php files
+    
+    $hostname = "localhost";
+    $dbUser = "projectapp";
+    $dbPassword = "P2ssw0rd";
+    $db = "project";
+
+    ini_set('display_errors',1); 
+
+?>
