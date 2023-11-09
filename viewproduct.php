a135bbc8fd3b0e4733bcbbc738cea606c55017ec
shuookang <149999283+shuookang@users.noreply.github.com>
2023-11-09 15:36:30 +0800

    Update viewproduct.php

--- viewproduct.php
+++ viewproduct.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
         require_once("dbinfo.php");
 
@@ -21,3 +29,7 @@
         $mysqli->close();
 
     ?>
+
+</body>
+</html>
+
