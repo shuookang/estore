90cac4e647cdf034dc0e5ee0fc2f6ec00d98fb84
feliciaaong <feliciaong444@gmail.com>
2023-11-09 15:20:20 +0800

    php

--- dbinfo.php
+++ dbinfo.php
@@ -3,10 +3,11 @@
     //Set up 4 global variables that can be included by other php files
     
     $hostname = "localhost";
-    $dbUser = "projectapp";
-    $dbPassword = "P2ssw0rd";
-    $db = "project";
+    $dbUser = "estore";
+    $dbPassword = "@estore123";
+    $db = "estore";
 
     ini_set('display_errors',1); 
+
 
 ?>
